124d8de8d8a9eda2009293bd443565b418bfb58e
Jonathan Hallström <lmj.hallstrom@gmail.com>
2026-09-06 19:50:00 +0200

    proper metrics
    and a simd impl

--- src/count.zig
+++ src/count.zig
@@ -4,33 +4,51 @@ const HEADER_SIZE = 32;
 const MOVE_SIZE = 4;
 const TERMINATOR: [4]u8 = @splat(0);
 
-pub fn count_games_slice(bytes: []align(4) const u8) usize {
+fn count_games_slice_scalar(bytes: []const u8) usize {
     var count: usize = 0;
 
     var i: usize = 0;
-    while (i < bytes.len) {
+    while (i + HEADER_SIZE + MOVE_SIZE <= bytes.len) {
         i += HEADER_SIZE;
+        count += 1;
 
-        while (i < bytes.len) {
-            i += MOVE_SIZE;
+        while (i + MOVE_SIZE <= bytes.len) : (i += MOVE_SIZE) {
             if (std.mem.eql(u8, bytes[i..][0..4], &TERMINATOR))
                 break;
-        } else {
-            break;
         }
-
-        count += 1;
     }
 
     return count;
 }
 
-pub const Count = struct {
-    games: usize,
-    positions: usize,
+pub fn count_games_slice(bytes: []align(4) const u8) usize {
+    var count: usize = 0;
+
+    var i: usize = 0;
+    while (i + HEADER_SIZE + MOVE_SIZE <= bytes.len) {
+        i += HEADER_SIZE;
+        count += 1;
+
+        const move_slice: []const u32 = @ptrCast(@alignCast(bytes[i..]));
+        const end_idx = std.mem.indexOfScalar(u32, move_slice, 0) orelse move_slice.len;
+
+        i += MOVE_SIZE * end_idx;
+    }
+
+    if (std.debug.runtime_safety)
+        std.debug.assert(count == count_games_slice_scalar(bytes));
+
+    return count;
+}
+
+pub const ParsedFile = struct {
+    games: u64,
+    positions: u64,
+    size: u64,
+    time: std.Io.Duration,
 };
 
-pub fn count_file(io: std.Io, f: std.Io.File) !Count {
+pub fn parse_file(io: std.Io, f: std.Io.File) !ParsedFile {
     const len = try f.length(io);
     var map = try f.createMemoryMap(io, .{
         .len = len,
@@ -39,11 +57,16 @@ pub fn count_file(io: std.Io, f: std.Io.File) !Count {
     defer map.destroy(io);
     try map.read(io);
 
+    const start = std.Io.Timestamp.now(io, .real);
     const games = count_games_slice(map.memory);
+    const time = start.untilNow(io, .real);
+
     const bytes = map.memory.len;
 
     return .{
         .games = games,
         .positions = (bytes - 36 * games) / 4,
+        .size = bytes,
+        .time = time,
     };
 }
--- src/main.zig
+++ src/main.zig
@@ -1,13 +1,15 @@
 const std = @import("std");
 
-const count = @import("count.zig");
+const viriformat_parsing = @import("count.zig");
 
 pub fn main(init: std.process.Init) !void {
+    const io = init.io;
+
+    const program_start = std.Io.Timestamp.now(io, .real);
+
     const allocator = init.gpa;
     var args = try init.minimal.args.iterateAllocator(allocator);
     defer args.deinit();
-
-    const io = init.io;
 
     var files = std.array_list.Managed(std.Io.File).init(allocator);
     defer files.deinit();
@@ -24,35 +26,52 @@ pub fn main(init: std.process.Init) !void {
         }
     }
 
-    const CountResult = @typeInfo(@TypeOf(count.count_file)).@"fn".return_type.?;
+    const CountResult = @typeInfo(@TypeOf(viriformat_parsing.parse_file)).@"fn".return_type.?;
 
     var futures = std.array_list.Managed(std.Io.Future(CountResult)).init(allocator);
     defer futures.deinit();
 
     for (files.items) |file| {
-        try futures.append(io.async(count.count_file, .{ io, file }));
+        try futures.append(io.async(viriformat_parsing.parse_file, .{ io, file }));
     }
 
-    var total_games: usize = 0;
-    var total_positions: usize = 0;
+    var total_games: u64 = 0;
+    var total_positions: u64 = 0;
+    var parsing_time = std.Io.Duration.zero;
+    var total_size: u64 = 0;
 
     for (futures.items) |*future| {
         if (future.await(io)) |res| {
             total_games += res.games;
             total_positions += res.positions;
+            parsing_time.nanoseconds += res.time.nanoseconds;
+            total_size += res.size;
         } else |err| {
             std.log.err("{}", .{err});
         }
     }
 
+    const elapsed_ns: u96 = @intCast(parsing_time.toNanoseconds());
+
     const stdout_file = std.Io.File.stdout();
     var stdout_buf: [4096]u8 = undefined;
     var stdout = stdout_file.writer(io, &stdout_buf);
 
+    const total_time = program_start.untilNow(io, .real);
+
     try stdout.interface.print(
-        \\total games: {}
+        \\total games:     {}
         \\total positions: {}
+        \\parsing time:    {f}
+        \\total time       {f}
+        \\speed:           {Bi:.2}/s
         \\
-    , .{ total_games, total_positions });
+    , .{
+        total_games,
+        total_positions,
+        parsing_time,
+        total_time,
+        @as(u64, @intCast(@as(u128, total_size) * std.time.ns_per_s / elapsed_ns)),
+    });
     try stdout.flush();
 }
